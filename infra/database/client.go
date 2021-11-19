5f007d51564a100ea3cc7c540ca2f4912bc616cb
Edson Cabral Junior <e2cabraljunior@gmail.com>
2021-11-19 20:08:18 -0300

    🚧 create a database client

---
+++ infra/database/client.go
@@ -0,0 +1,25 @@
+package database
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func Connect() *mongo.Client {
+	clientOptions := options.Client().ApplyURI("")
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = client.Ping(context.TODO(), nil)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return client
+}
